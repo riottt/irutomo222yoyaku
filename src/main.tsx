d6c0c7de9507c3e38f6416cf17ad65cbc6bae50c
riottt <rio07@rio.com>
2025-02-25 22:40:22 +0900

    feat: PayPalの決済機能を実装

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PayPalScriptProvider options={{
+      "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID,
+      currency: "USD",
+      intent: "capture"
+    }}>
+      <App />
+    </PayPalScriptProvider>
   </StrictMode>
 );
